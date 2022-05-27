2fe3c52eb964a1632dcd9b852285d35e7867101e
那份天空 <zhuo75822@163.com>
2022-05-27 17:08:07 +0800

    chore: 增加test测试脚本

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 const App = () => {
-    console.log(1);
-    const change = ()=> {
-        try {
-            console.log("true");
-            throw new Error('sdasd')
-        } catch (error) {
-            console.log(error);
-        }
+  console.log(1);
+  const change = () => {
+    try {
+      console.log('true');
+      throw new Error('sdasd');
+    } catch (error) {
+      console.log(error);
     }
-    return <div onClick={change}>this is a react app</div>;
+  };
+  return <div onClick={change}>this is a react app</div>;
 };
 export default App;
